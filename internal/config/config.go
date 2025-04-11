469a9b310da493d1fa969b7138f17495593d54e1
Liedsonfsa <fabricoliedson@gmail.com>
2025-04-11 09:20:29 -0300

    feat: adding config.go file

---
+++ internal/config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	ServerAddress string
+	DBConfig DBConfig
+	JWTSecret string
+}
+
+type DBConfig struct {
+	Host     string
+    Port     int
+    User     string
+    Password string
+    DBName   string
+}
+
+func LoadConfig() Config {
+	return Config{
+		ServerAddress: getEnv("SERVER_ADDRESS", ":8080"),
+        DBConfig: DBConfig{
+            Host:     getEnv("DB_HOST", "localhost"),
+            Port:     getEnvAsInt("DB_PORT", 5432),
+            User:     getEnv("DB_USER", "postgres"),
+            Password: getEnv("DB_PASSWORD", "postgres"),
+            DBName:   getEnv("DB_NAME", "task_manager"),
+        },
+        JWTSecret: getEnv("JWT_SECRET", "secret"),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultValue
+}
+
+func getEnvAsInt(key string, defaultValue int) int {
+	if value, exists := os.LookupEnv(key); exists {
+        if intValue, err := strconv.Atoi(value); err == nil {
+            return intValue
+        }
+    }
+
+    return defaultValue
+}
